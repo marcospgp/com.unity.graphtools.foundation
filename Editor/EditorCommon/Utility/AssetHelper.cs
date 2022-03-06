42eecf137d14d23158571e4eeaf1278a9513fe50
Marcos Pereira <marcospereiradev@gmail.com>
2022-03-06 19:50:59 +0000

    Fixes

--- Editor/EditorCommon/Utility/AssetHelper.cs
+++ Editor/EditorCommon/Utility/AssetHelper.cs
@@ -2,6 +2,6 @@ namespace UnityEditor.GraphToolsFoundation.Overdrive
 {
     static class AssetHelper
     {
-        internal const string AssetPath = "Assets/Graph Tools Foundation/No longer editor/";
+        internal const string AssetPath = "Assets/Graph Tools Foundation/Editor/";
     }
 }
